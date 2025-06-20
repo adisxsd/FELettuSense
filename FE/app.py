867fadfccee3f8d18d068dffacf56f8c9f08026d
Nurul Hadits <117652857+adisxsd@users.noreply.github.com>
2025-06-20 11:11:38 +0700

    Update app.py

--- FE/app.py
+++ FE/app.py
@@ -99,5 +99,9 @@ def riwayat():
         flash('Gagal memuat riwayat.')
         return render_template('riwayat.html', riwayat=[], backend_url=BACKEND_URL)
 
+
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
